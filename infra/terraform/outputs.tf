e37598a70577472fc541c90c2f33820588848fc2
deepakbatra5 <deepakkumar9798919579@gmail.com>
2025-12-22 23:02:02 +0530

    price changed

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -3,7 +3,7 @@
 
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.-app_server.id
+  value       = aws_instance.app_server.id
 }
 
 output "instance_public_ip" {
